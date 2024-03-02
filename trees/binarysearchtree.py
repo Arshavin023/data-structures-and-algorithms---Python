97495c6ddd63d1a8af7418204a2b80225e5d24e7
Arshavin023 <uchejudennodim@gmail.com>
2024-03-02 11:48:27 +0100

    binary search delete operation implemented with max and min

--- trees/binarysearchtree.py
+++ trees/binarysearchtree.py
@@ -67,12 +67,51 @@ class BinarySearchTreeNode:
             sum += self.right.find_sum()
         return sum
 
-    def delete_value(self, data):
-        pass
+    # delete using min value
+    def delete_with_min(self, value):
+        if value < self.data:
+            if self.left:
+                self.left = self.left.delete_with_min(value)
+        elif value > self.data:
+            if self.right:
+                self.right = self.right.delete_with_min(value)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.right
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = self.right.delete_with_min(min_val)
 
+        return self
+    
+    # delete using max value
+    def delete_with_max(self, value):
+        if value < self.data:
+            if self.left:
+                self.left = self.left.delete_with_max(value)
+        elif value > self.data:
+            if self.right:
+                self.right = self.right.delete_with_max(value)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.right is None:
+                return self.left
+            elif self.left is None:
+                return self.right
+            max_val = self.left.find_min()
+            self.data = max_val
+            self.left = self.left.delete_with_max(max_val)
+
+        return self
         
 # Utilizing the BinarySearchTreeNode
 def build_tree(elements):
+    print(f"Building tree with these list {elements}")
     root = BinarySearchTreeNode(elements[0])
     for i in range(1,len(elements)):
         root.add_child(elements[i])
@@ -81,15 +120,18 @@ def build_tree(elements):
 
 if __name__ == '__main__':
     # countries = ["india","Pakistan","Germany","USA","China","India","UK","USA"]
-    # numbers = [random.randint(1,100) for _ in range(1,100)]
-    numbers = [1,2,3,4,5]
+    numbers = [random.randint(1,100) for _ in range(1,100)]
+    # numbers = [2,1,3,4,5,7,4]
     numbers_tree = build_tree(numbers)
     # countries_tree = build_tree(countries)
     # print(countries_tree.in_order_traversal())
     # print(f'Is Nigeria is in the list: {countries_tree.search("Nigeria")}')
-    print(numbers_tree.in_order_traversal())
-    print(numbers_tree.search(random.randint(1,100)))
-    print(numbers_tree.find_min())
-    print(numbers_tree.find_max())
-    print(numbers_tree.find_sum())
-
+    # print(numbers_tree.in_order_traversal())
+    # print(numbers_tree.search(random.randint(1,100)))
+    # print(numbers_tree.find_min())
+    # print(numbers_tree.find_max())
+    # print(numbers_tree.find_sum())
+    number_to_delete = int(input("enter number you desire to delete: "))
+    # print(numbers_tree.delete_with_max(number_to_delete))
+    print(numbers_tree.delete_with_min(number_to_delete))
+    print(f"After deleting number {numbers_tree.in_order_traversal()}")
